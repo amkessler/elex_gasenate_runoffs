f3fcdeac76fc32e5476ff7609f1603c2d649328f
Kessler <ak113714@bna.com>
2020-12-03 11:39:29 -0500

    initial commit

---
+++ 01_load_data.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+
+# ACTBLUE FILINGS ####
+
+# contribs
+
+#import zipped file 
+actblue_contribs_all <- read_csv("raw_data/actblue_oct_monthly_sa1464847.csv.zip", col_types = cols(.default = "c"))
+
+#format columns and add name column
+actblue_contribs_all <- actblue_contribs_all %>% 
+  mutate(
+    amount = as.numeric(amount),
+    aggregate_amount = as.numeric(aggregate_amount),
+    date = ymd(date),
+    cmte_name = "ACTBLUE"
+  ) %>% 
+  select(cmte_name, everything())
+
+glimpse(actblue_contribs_all)
+
+#filter for only records after a specific date
+actblue_contribs_selectdates <- actblue_contribs %>% 
+  filter(date > "2020-09-29")
+
+#save for next steps
+saveRDS(actblue_contribs_selectdates, "processed_data/actblue_contribs_selectdates.rds")
