2c93b5e2d91b247ce9d677cda8cf98cdd293e47f
Kessler <ak113714@bna.com>
2020-12-04 00:11:35 -0500

    test out real cmtes for ga

--- 03_cmtes_from_api.R
+++ 03_cmtes_from_api.R
@@ -8,20 +8,25 @@ library(janitor)
 library(tidyusafec)
 options(scipen = 999)
 
+# CMTES of INTEREST:
+# C00571703 - SENATE LEADERSHIP FUND
+# C00484642 - SMP (Senate Majority PAC)
+
+
 #save api key for use here
 save_datagov_apikey(key = Sys.getenv("FECAPIKEY"))
 
 
 #get committee reports - starting with one single committee
-testcmte <- get_committee_reports(committee_id = "C00492454", 
+testcmte <- get_committee_reports(committee_id = "C00571703", 
                                   cycle = 2020)
 
 
 #get CONTRIBS - starting with one single committee
-testcmte_contribs <- get_itemized_contributions(committee_id = "C00492454", 
-                                                min_amount = 2000,
-                                                is_individual = TRUE,
-                                                min_date = "2020-06-30")
+testcmte_contribs <- get_itemized_contributions(committee_id = "C00571703", 
+                                                # min_amount = 2000,
+                                                # is_individual = TRUE,
+                                                min_date = "2020-11-03")
 
 
 glimpse(testcmte_contribs)
